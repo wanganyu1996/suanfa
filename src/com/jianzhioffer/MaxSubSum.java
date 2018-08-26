64b9151d795a9d93e8270fa37d53b57fc66b225a
wanganyu <2508635478@qq.com>
2018-08-26 16:27:51 +0800

    连续子数组最大和

--- src/com/jianzhioffer/MaxSubSum.java
+++ src/com/jianzhioffer/MaxSubSum.java
@@ -4,6 +4,18 @@ package com.jianzhioffer;
  * 连续子数组的最大和
  */
 public class MaxSubSum {
+    /**链接：https://www.nowcoder.com/questionTerminal/459bd355da1549fa8a49e350bf3df484
+    来源：牛客网 方法二
+**/
+    public  int FindGreatestSumOfSubArray(int[] array) {
+        int res = array[0]; //记录当前所有子数组的和的最大值
+        int max=array[0];   //包含array[i]的连续数组最大值
+        for (int i = 1; i < array.length; i++) {
+            max=Math.max(max+array[i], array[i]);
+            res=Math.max(max, res);
+        }
+        return res;
+    }
     /*
   求三个数中的最大值
   */
