157389e3dcd1b88d844a67e7bea73b9458d92317
wanganyu1996 <2508635478@qq.com>
2019-02-05 02:16:09 +0800

    序列化二叉树

---
+++ src/com/jianzhioffer/serializeAndDeserialize/Solution.java
@@ -0,0 +1,46 @@
+package com.jianzhioffer.serializeAndDeserialize;
+
+public class Solution {
+    private int index = -1;
+
+    String Serialize(TreeNode root) {
+        StringBuffer sb = new StringBuffer();
+        if (root == null) {
+            sb.append("#,");
+            return sb.toString();
+        }
+        sb.append(root.val + ",");
+        sb.append(Serialize(root.left));
+        sb.append(Serialize(root.right));
+        return sb.toString();
+    }
+
+    TreeNode Deserialize(String str) {
+        index++;
+        if(index>=str.length()){
+            return null;
+        }
+        TreeNode node = null;
+        String strr[] = str.split(",");
+        if(!strr[index].equals("#")){
+            node = new TreeNode(Integer.valueOf(strr[index]));
+            node.left = Deserialize(str);
+            node.right = Deserialize(str);
+        }
+        return node;
+    }
+
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+}
+
+
